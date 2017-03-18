b8da791d58ecabdbcb524138b1e31143c1e43c92
Gustavo Otero <gustavo@chimera.digital>
2017-03-18 10:51:22 -0500

    namespace ColorPicker to avoid conflicts with other classes project organization camera setup to be used with other cameras in scene without affect them

---
+++ Assets/ColorPicker/Scripts/Controls/BaseControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+namespace ColorPicker
+{
+    public abstract class BaseControl : MonoBehaviour
+    {
+
+        protected bool Pressed;
+        protected Vector2 PressPosition;
+        protected Vector2 Offset;
+
+        public virtual void Select(Vector2 pressPosition)
+        {
+            Pressed = true;
+            PressPosition = pressPosition;
+        }
+
+        public virtual void Reset()
+        {
+            Pressed = false;
+            PressPosition = Vector2.zero;
+        }
+
+        public abstract void Submit();
+        public abstract void Refresh();
+
+    }
+}
